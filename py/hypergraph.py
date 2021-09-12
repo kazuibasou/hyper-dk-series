6866052195a8e94e0e1e15812a991cb84b56a354
kazuibasou <kazuki@kazuki-nakajima-macbook-pro-2019.local>
2021-09-12 06:16:07 -0400

    updated hypergraph.py

--- py/hypergraph.py
+++ py/hypergraph.py
@@ -1,19 +1,27 @@
 from collections import defaultdict
 
 class HyperGraph():
-    V = []
-    E = []
-    elist = defaultdict(list)
+    V = [] # A list of nodes.
+    E = [] # A list of hyperedges.
+    elist = {} # A dictionary of lists of indices in the list E of hyperedges to which each node belongs.
+
+    # Example
+    # V = [1, 2, 3, 4, 5]
+    # E = [[1, 2], [2, 3], [1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]]
+    # elist = {1: [0, 2, 3, 4], 2: [0, 1, 2, 3, 4], 3: [1, 2, 3, 4], 4: [3, 4], 5: [4]}
+    # In this example, elist[1] = [0, 2, 3, 4] implies that node 1 belongs to hyperedges E[0], E[2], E[3], and E[4].
 
     def __init__(self):
         self.V = []
         self.E = []
         self.elist = {}
 
-    def read_hypergraph(self, graph_name):
-        f1_path = "../data/" + str(graph_name) + "_nverts.txt"
+    def read_hypergraph(self, hypergraph_name):
+        # read hypergraph named hypergraph_name
+
+        f1_path = "../data/" + str(hypergraph_name) + "_nverts.txt"
         f1 = open(f1_path, 'r')
-        f2_path = "../data/" + str(graph_name) + "_hyperedges.txt"
+        f2_path = "../data/" + str(hypergraph_name) + "_hyperedges.txt"
         f2 = open(f2_path, 'r')
 
         lines1 = f1.readlines()
@@ -42,13 +50,15 @@ class HyperGraph():
         f1.close()
         f2.close()
 
-        print('The given hypergraph named ' +str(graph_name) + ' was successfully read.')
+        print('The given hypergraph named ' +str(hypergraph_name) + ' was successfully read.')
         print("Number of nodes: ", len(self.V))
         print("Number of hyperedges: ", len(self.E), "\n")
 
         return 0
 
     def add_node_to_hyperedge(self, v, e_i):
+        #Add node v to hyperedge E[e_i]
+
         if v not in self.elist:
             print("Error: Given node is not found.")
             exit()
@@ -62,6 +72,8 @@ class HyperGraph():
         return 0
 
     def remove_node_from_hyperedge(self, v, e_i):
+        #Remove node v from hyperedge E[e_i]
+
         if v not in self.elist:
             print("Error: Given node is not found.")
             exit()
@@ -82,6 +94,7 @@ class HyperGraph():
         return 0
 
     def node_degree(self):
+        # Calculate the degree of each node (i.e., the number of hyperedges to which each node belongs).
 
         nd = defaultdict(int)
         for v in self.V:
@@ -90,6 +103,8 @@ class HyperGraph():
         return nd
 
     def maximum_node_degree(self):
+        # Calculate the maximum degree of the node.
+
         mnd = 0
         for v in self.V:
             k = int(len(self.elist[v]))
@@ -99,6 +114,7 @@ class HyperGraph():
         return mnd
 
     def num_jnt_node_deg(self):
+        # Calculate the number of hyperedges that nodes with degree k and nodes with degree k' share.
         jnd = defaultdict(lambda: defaultdict(int))
 
         for e in self.E:
@@ -115,6 +131,10 @@ class HyperGraph():
         return jnd
 
     def node_redundancy_coefficient(self):
+        # Calculate the redundancy coefficient of each node.
+        # See the following paper for the detail of the redundancy coefficient.
+        # Basic notions for the analysis of large two-mode networks, M. Latapy, C. Magnien, N. Del Vecchio, Social networks, 2008.
+        
         rc = defaultdict(float)
         nd = self.node_degree()
 
@@ -138,6 +158,8 @@ class HyperGraph():
         return rc
 
     def degree_dependent_node_redundancy_coefficient(self):
+        # Calculate The degree-dependent redundancy coefficient of the node (i.e., the average of the redundancy coefficient over the nodes with degree k).
+
         rc = self.node_redundancy_coefficient()
         nd = self.node_degree()
 
@@ -156,6 +178,8 @@ class HyperGraph():
         return ddrc
 
     def hyperedge_size(self):
+        # Calculate the size of each hyperedge (i.e., the number of nodes that belong to each hyperedge).
+
         hs = defaultdict(int)
 
         for e_i in range(0, len(self.E)):
