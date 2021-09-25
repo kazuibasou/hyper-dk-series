942d9e96c39a760d73f9d1a8ae0791c94f9b9976
kazuibasou <kazuki@kazuki-nakajima-macbook-pro-2019.local>
2021-09-25 11:04:59 -0400

    updated .gitignore

--- cpp/src/rewiring.h
+++
@@ -1,7 +0,0 @@
-#ifndef REWIRING_H
-#define REWIRING_H
-
-HyperGraph targeting_rewiring_d_v_two(HyperGraph G, HyperGraph rand_G);
-HyperGraph targeting_rewiring_d_v_two_five(HyperGraph G, HyperGraph rand_G);
-
-#endif
